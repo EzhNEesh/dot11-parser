94aab6ab3cdca9e71822ac5b90d6526bdf8f40b5
EzhNEesh <oyaiskander@yandex.ru>
2024-01-24 14:17:32 +0300

    fix: frame_data type fixed

--- src/finder.py
+++ src/finder.py
@@ -1,21 +1,19 @@
 class Finder:
     def __init__(self):
-        self.frame_data = []
+        self.frame_data = {}
 
     def parse_data(self, file_path: str):
         with open(file_path) as file:
             for line in file:
                 meta = line[11:].split(',')
                 current_frame = {
-                    'id': line[:10],
                     'offset': meta[0][7:],
                     'BW': meta[1][3:],
                     'MCS': meta[2][4:],
                     'size': meta[3][5:],
                     'bits': meta[4][5:-1]
                 }
-                self.frame_data.append(current_frame)
-        print(*self.frame_data[:100], sep='\n')
+                self.frame_data[line[10:]] = current_frame
 
 
 finder = Finder()

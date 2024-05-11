49c679870f099cfe2af8ef0edbc89b0145d759b8
EzhNEesh <oyaiskander@yandex.ru>
2024-05-12 01:43:14 +0300

    fix: GUI buttons disabled

--- src/main.py
+++ src/main.py
@@ -10,16 +10,19 @@ def main(page: ft.Page):
     page.title = "Drone detector"
     action_container = ft.Container()
 
-    input_path = ft.TextField(label="Enter path to input data file", width=300, height=35, text_size=14)
-    output_path = ft.TextField(label="Enter path to output file", value="", width=300, height=35, text_size=14)
-    ssid = ft.TextField(label="Enter ssid for search", value="", width=300, height=35, text_size=14)
+    def update_find_button(e):
+        if len(input_path.value):
+            find_button.disabled = False
+        else:
+            find_button.disabled = True
+        action_container.update()
 
-    paths_to_datas = ft.TextField(label="Enter paths to data files with line break", multiline=True)
-    drone_addresses = ft.TextField(label="Enter MAC addresses with line break", multiline=True)
-    rg_models = ft.RadioGroup(content=ft.Column([
-        ft.Radio(value="knn", label="K-Neighbors Classifier"),
-        ft.Radio(value="logistic_regression", label="Logistic Regression"),
-        ft.Radio(value="decision_tree", label="Decision Tree")]))
+    def update_retrain_button(e):
+        if len(paths_to_datas.value) and len(drone_addresses.value):
+            retrain_button.disabled = False
+        else:
+            retrain_button.disabled = True
+        action_container.update()
 
     def find_drones(e):
         dot11_dataframe = handler.read_frames(input_path.value)
@@ -32,7 +35,7 @@ def main(page: ft.Page):
         else:
             prediction, macs, control_devices = handler.predict(rg_models.value, dot11_dataframe)
         network_graph.draw_graph(dot11_dataframe, prediction, macs, control_devices)
-        if output_path.value:
+        if len(output_path.value):
             handler.save_to_file(dot11_dataframe, macs, output_path.value)
         else:
             handler.save_to_file(dot11_dataframe, macs)
@@ -45,11 +48,11 @@ def main(page: ft.Page):
     def add_predict_to_page():
         action_container.content = ft.Column(
             [
-                ft.Row([ft.Text("Path to input data file", size=14), input_path], spacing=10),
+                ft.Row([ft.Text("Path to input data file*", size=14), input_path], spacing=10),
                 ft.Row([ft.Text("Path to output data file", size=14), output_path], spacing=10),
                 ft.Row([ft.Text("SSID", size=14), ssid], spacing=10),
                 rg_models,
-                ft.ElevatedButton(text="Start Searching", on_click=find_drones)
+                find_button
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             spacing=20)
@@ -61,7 +64,7 @@ def main(page: ft.Page):
                 paths_to_datas,
                 drone_addresses,
                 rg_models,
-                ft.ElevatedButton(text="Retrain", on_click=retrain_model)
+                retrain_button
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             spacing=20)
@@ -75,13 +78,21 @@ def main(page: ft.Page):
                 add_retrain_to_page()
             case 'Incremental train':
                 pass
-        # page.update()
 
-    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    input_path = ft.TextField(label="Enter path to input data file",
+                              width=300, height=35,
+                              text_size=14, on_change=update_find_button)
+    output_path = ft.TextField(label="Enter path to output file", value="", width=300, height=35, text_size=14)
+    ssid = ft.TextField(label="Enter ssid for search", value="", width=300, height=35, text_size=14)
 
-    # def minus_click(e):
-    #     txt_number.value = str(int(txt_number.value) - 1)
-    #     page.update()
+    paths_to_datas = ft.TextField(label="Enter paths to data files with line break*", multiline=True,
+                                  on_change=update_retrain_button)
+    drone_addresses = ft.TextField(label="Enter MAC addresses with line break*", multiline=True,
+                                   on_change=update_retrain_button)
+    rg_models = ft.RadioGroup(content=ft.Column([
+        ft.Radio(value="knn", label="K-Neighbors Classifier"),
+        ft.Radio(value="logistic_regression", label="Logistic Regression"),
+        ft.Radio(value="decision_tree", label="Decision Tree")]))
 
     rg = ft.RadioGroup(content=ft.Column([
         ft.Radio(value="Find", label="Find"),
@@ -91,5 +102,7 @@ def main(page: ft.Page):
     page.add(rg)
     page.add(action_container)
 
+    find_button = ft.ElevatedButton(text="Start Searching", on_click=find_drones, disabled=True)
+    retrain_button = ft.ElevatedButton(text="Retrain", on_click=retrain_model, disabled=True)
 
 ft.app(main)

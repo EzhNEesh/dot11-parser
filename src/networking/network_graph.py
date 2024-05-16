6f10a5f96e81f9369edf0afd4d0a8e0b911d2de6
EzhNEesh <oyaiskander@yandex.ru>
2024-05-16 22:14:21 +0300

    fix: sequential drawing of graphs form different files

--- src/networking/network_graph.py
+++ src/networking/network_graph.py
@@ -3,16 +3,16 @@ import matplotlib.pyplot as plt
 
 
 class NetworkGraph:
-    graph = nx.Graph()
-
-    def draw_graph(self, dot11_dataframe, prediction, macs, control_devices):
+    @staticmethod
+    def draw_graph(dot11_dataframe, prediction, macs, control_devices):
+        graph = nx.Graph()
         for ind, row in dot11_dataframe.frames.iterrows():
             if row['src_address'] is not None and row['dst_address'] is not None:
-                self.graph.add_edge(row['src_address'], row['dst_address'])
+                graph.add_edge(row['src_address'], row['dst_address'])
         drones_macs = dot11_dataframe.get_macs_by_prediction(prediction, macs)
 
         color_map = []
-        for node in self.graph:
+        for node in graph:
             if node in drones_macs:
                 color_map.append('red')
             elif node in control_devices:
@@ -21,5 +21,5 @@ class NetworkGraph:
                 color_map.append('green')
             # color_map = ['red' if node in drones_macs else 'green' for node in self.graph]
         # pos = nx.spectral_layout(self.graph)
-        nx.draw(self.graph, node_color=color_map, with_labels=True)
+        nx.draw(graph, node_color=color_map, with_labels=True)
         plt.show()
